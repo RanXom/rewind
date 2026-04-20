e7846f880f14c1836c0751b2ad9640fde0681cbd
RanXom <saiyedshizain@gmail.com>
2026-04-20 13:58:12 +0530

    implement an enum for commands
    implement a struct cli for storing and handling the captured command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{ Parser, Subcommand };
+
+#[derive(Parser)]
+#[command(name = "rw")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Log {
+        #[arg(short, long)]
+        command: String,
+
+        #[arg(short, long)]
+        exit: i64,
+
+        #[arg(short, long)]
+        cwd: String,
+
+        #[arg(short, long)]
+        start: i64
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Log { command, exit, cwd, start } => {
+            println!("rw debug:");
+            println!("Captured: \"{}\"", command);
+            println!("Status: {}", if *exit == 0 { "finish with 0" } else { "failure" });
+            println!("Working dir: {}", cwd);
+            println!("Started: {}", start);
+        }
+    }
 }
